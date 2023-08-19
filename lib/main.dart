0c2ad11931f42e66802bb49bdd3d7c3e6658fb88
Junior Alves <junioralves.webdeveloper@gmail.com>
2023-08-19 14:14:43 -0300

    feat(Section 2): Applying style to the question widget

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Question(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: _responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: () {
+                print('Resposta 2 ta ok!');
+              },
+            ),
+            ElevatedButton(
+                child: Text('Resposta 3'),
+                onPressed: () => print('Resposta 3 ta funfando')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaApp({Key? key}) : super(key: key);
+
+  @override
+  State<PerguntaApp> createState() {
+    return _PerguntaAppState();
+  }
+}
