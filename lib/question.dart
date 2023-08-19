d1f12d845c8ddf279780e75577875126a56fca79
Junior Alves <junioralves.webdeveloper@gmail.com>
2023-08-19 19:46:06 -0300

    feat(Section 2): Final section

--- lib/question.dart
+++ lib/question.dart
@@ -9,10 +9,10 @@ class Question extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(20),
       child: Text(
         text,
-        style: const TextStyle(fontSize: 28),
+        style: const TextStyle(fontSize: 24),
         textAlign: TextAlign.center,
       ),
     );
--- lib/responses.dart
+++ lib/responses.dart
@@ -8,15 +8,33 @@ class Responses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: ElevatedButton(
-        child: Text(
-          text,
-          style: const TextStyle(fontSize: 20),
+    return Column(
+      children: [
+        Container(
+          width: 300,
+          child: ElevatedButton(
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 25,
+                color: Color.fromARGB(255, 161, 229, 254),
+              ),
+            ),
+            onPressed: onSelection,
+            style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(255, 4, 23, 67),
+                onPrimary: const Color.fromARGB(255, 7, 90, 255),
+                elevation: 20, // Elevation
+                shadowColor: const Color.fromARGB(255, 54, 164, 255),
+                padding: const EdgeInsets.all(25)
+                // Shadow Color
+                ),
+          ),
         ),
-        onPressed: onSelection,
-      ),
+        const SizedBox(
+          height: 30,
+        )
+      ],
     );
   }
 }
